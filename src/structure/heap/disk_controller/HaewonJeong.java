4f118e09225defbd2a9d678ca0918d440d26d650
HaewonJeong <bonjour_haewon@naver.com>
2026-05-10 21:46:00 +0900

    더 맵게 제출

---
+++ src/structure/heap/disk_controller/HaewonJeong.java
@@ -0,0 +1,20 @@
+package structure.heap.disk_controller;
+import java.util.PriorityQueue;
+
+public class HaewonJeong {
+    /*
+    public static void main(String[] args) {
+        HaewonJeong hw = new HaewonJeong();
+        int[][] jobs = {{0, 3}, {1, 9}, {3, 5}};
+        int K = 7;
+        System.out.println(hw.solution(jobs));
+    }
+
+    public int solution(int[][] jobs) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //푸는중
+
+        int count = 0;
+        return count;
+    }*/
+}
